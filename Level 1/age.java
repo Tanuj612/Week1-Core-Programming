cac73b08dace3247bf196aa2157cc961b21fab83
Tanuj612 <tanujmittal2018@gmail.com>
2025-06-12 08:48:22 +0530

    Add age.java file to calculate age in 2024

---
+++ Level 1/age.java
@@ -0,0 +1,11 @@
+// package Level 1;
+import java.util.Scanner;
+
+public class age {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int age=sc.nextInt();
+        System.out.println("Your age in 2024: " + (2024 - age));
+    }
+    
+}
